9c0108c98e1066dc3b842bbfb2d34d62c03ad3ba
Shirfuleak <84619665+Shirfuleak@users.noreply.github.com>
2022-11-08 21:02:25 +0530

    portfolio-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-import ToDoList from "./Components/ToDoList";
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Experience from './Components/Experiance';
+import Projects from './Components/Projects';
+import Footer from './Components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Contact from './Components/Contact';
 
 function App() {
-
   return (
-    <div className="App">
-        <ToDoList></ToDoList>
-    </div>
+    <>
+   
+  {/* <Home></Home> */}
+  <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/project" element={<Projects></Projects>}></Route>
+        <Route path='/Experiance' element={<Experience></Experience>}></Route>
+    </Routes>
+
+     
+    </>
   );
 }
 

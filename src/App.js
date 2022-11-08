2d2e35c66db2b1253be47be11c961bdcc2e04de3
Shirfuleak <84619665+Shirfuleak@users.noreply.github.com>
2022-11-08 21:04:08 +0530

    table-formik

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import Experience from './Components/Experiance';
-import Projects from './Components/Projects';
-import Footer from './Components/Footer';
-import { Route, Routes } from 'react-router-dom';
-import Contact from './Components/Contact';
+import Formik from './sept19/Registration';
+import Tablature from './sept20/Tablefeature';
 
 function App() {
   return (
-    <>
-   
-  {/* <Home></Home> */}
-  <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/contact" element={<Contact></Contact>}></Route>
-        <Route path="/project" element={<Projects></Projects>}></Route>
-        <Route path='/Experiance' element={<Experience></Experience>}></Route>
-    </Routes>
-
-     
-    </>
+    <div className="App">
+      <Formik></Formik> 
+      <Tablature></Tablature>
+    </div>
   );
 }
 

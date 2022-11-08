68bb9c207b46b6078c574c0857295893ea084aa4
Shirfuleak <84619665+Shirfuleak@users.noreply.github.com>
2022-11-08 20:59:01 +0530

    expense-tracker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import Animation  from './Components/Animation';
-import { motion } from 'framer-motion'
-function App() {
+import React from "react";
+import Homecomp from "./Components/Home";
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+  background-color: aqua;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+  align-items: center;
+  height: 100vh;
+  width: 98%;
+  padding-top: 30px ;
+  font-family: Montserrat;
+`;
+
+const Header = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 22px;
+  font-size: 25px;
+  font-weight: bold;
+`;
+const App = () => {
   return (
-    <div >
-     <Animation></Animation>
-    </div>
+    <Container>
+      <Header>Expense Tracker</Header>
+      <Homecomp></Homecomp>
+    </Container>
   );
-}
+};
 
 export default App;

0237e5dda390bc37402f719001ae10ebb8e85533
Shirfuleak <84619665+Shirfuleak@users.noreply.github.com>
2022-11-16 16:56:50 +0530

    Movie Webiste

--- src/schemas/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as Yup from 'yup';
-
-export const SignupSchema=Yup.object({
-    name:Yup.string().min(2).max(8).required("Enter your name"),
-    email:Yup.string().email().required("Enter your email"),
-    password:Yup.string().min(6).required("Enter Your password"),
-    cpassword:Yup.string().required().oneOf([Yup.ref("password"),null],"Password must be match")
-})
